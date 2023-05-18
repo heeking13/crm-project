d1eb254b66e354e0e55b00fd34980713cbc3bfa2
heqing <heqing2022@airchina.com>
2023-05-18 10:51:14 +0800

    mybatis mapper编写 service编写 service实现编写

---
+++ crm/src/main/java/com/bjpowernode/crm/settings/service/UserService.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.crm.settings.service;
+
+import com.bjpowernode.crm.settings.domain.User;
+
+import java.util.Map;
+import java.util.Objects;
+
+public interface UserService {
+    User queryUserByLoginActAndPwd(Map<String, Object> map);
+}
