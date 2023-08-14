8d1885129af45f73dfb6a8fd0f75dc74a7706605
heqing <heqing2022@airchina.com>
2023-08-14 15:50:41 +0800

    创建线索后台完成

--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ClueController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ClueController.java
@@ -1,14 +1,23 @@
 package com.bjpowernode.crm.workbench.web.controller;
 
+import com.bjpowernode.crm.commons.contants.Contants;
+import com.bjpowernode.crm.commons.domain.ReturnObject;
+import com.bjpowernode.crm.commons.utils.DateUtils;
+import com.bjpowernode.crm.commons.utils.UUIDUtils;
 import com.bjpowernode.crm.settings.domain.DicValue;
 import com.bjpowernode.crm.settings.domain.User;
 import com.bjpowernode.crm.settings.service.DicValueService;
 import com.bjpowernode.crm.settings.service.UserService;
+import com.bjpowernode.crm.workbench.domain.Clue;
+import com.bjpowernode.crm.workbench.service.ClueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -18,6 +27,8 @@ public class ClueController {
     private UserService userService;
     @Autowired
     private DicValueService dicValueService;
+    @Autowired
+    private ClueService clueService;
 
     @RequestMapping("/workbench/clue/index.do")
     public String index(HttpServletRequest request){
@@ -30,5 +41,29 @@ public class ClueController {
         request.setAttribute("clueStateList",clueStateList);
         request.setAttribute("sourceList",sourceList);
         return "workbench/clue/index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/workbench/clue/insertClue.do")
+    public Object insertClue(Clue clue, HttpSession session){
+        User user = (User) session.getAttribute(Contants.SESSION_USER);
+        clue.setId(UUIDUtils.getUUID());
+        clue.setCreateTime(DateUtils.formatDateTime(new Date()));
+        clue.setCreateBy(user.getId());
+        ReturnObject ro = new ReturnObject();
+        try{
+            int count = clueService.insertClue(clue);
+            if(count > 0){
+                ro.setCode(Contants.RETURN_RETURN_CODE_SUCCESS);
+            } else {
+                ro.setCode(Contants.RETURN_RETURN_CODE_FAIL);
+                ro.setMessage("系统繁忙，请稍后重试~");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            ro.setCode(Contants.RETURN_RETURN_CODE_FAIL);
+            ro.setMessage("系统繁忙，请稍后重试~");
+        }
+        return ro;
     }
 }
