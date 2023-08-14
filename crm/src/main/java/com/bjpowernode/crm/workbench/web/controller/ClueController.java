5554c3b99f53e9fce098d9041c33959d676d1b16
heqing <heqing2022@airchina.com>
2023-08-14 14:17:27 +0800

    线索页面跳转后台

---
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ClueController.java
@@ -0,0 +1,34 @@
+package com.bjpowernode.crm.workbench.web.controller;
+
+import com.bjpowernode.crm.settings.domain.DicValue;
+import com.bjpowernode.crm.settings.domain.User;
+import com.bjpowernode.crm.settings.service.DicValueService;
+import com.bjpowernode.crm.settings.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class ClueController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private DicValueService dicValueService;
+
+    @RequestMapping("/workbench/clue/index.do")
+    public String index(HttpServletRequest request){
+        List<User> userList = userService.queryAllUsers();
+        List<DicValue> appellationList = dicValueService.queryDicValueByTypeCode("appellation");
+        List<DicValue> clueStateList = dicValueService.queryDicValueByTypeCode("clueState");
+        List<DicValue> sourceList = dicValueService.queryDicValueByTypeCode("source");
+        request.setAttribute("userList",userList);
+        request.setAttribute("appellationList",appellationList);
+        request.setAttribute("clueStateList",clueStateList);
+        request.setAttribute("sourceList",sourceList);
+        return "workbench/clue/index";
+    }
+}
